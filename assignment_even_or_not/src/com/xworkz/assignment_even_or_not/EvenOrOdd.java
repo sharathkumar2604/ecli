bb30fda8ca26dc4646e4b771d8972787c0764df1
sharathkumar2604 <sharathkumargs.xworkz@gmail.com>
2022-11-09 01:45:04 +0530

    even or odd number conditional statement

---
+++ assignment_even_or_not/src/com/xworkz/assignment_even_or_not/EvenOrOdd.java
@@ -0,0 +1,19 @@
+package com.xworkz.assignment_even_or_not;
+
+public class EvenOrOdd {
+public static void main(String[] args )
+{
+	int num=9;
+	
+	if(num % 2==0)
+	{
+		System.out.println("even number");
+		
+	}
+	else
+		
+	{
+		System.out.println("odd number");
+	}
+}
+}

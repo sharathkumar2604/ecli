3164040e318e26b16d590639be64b644f5b0f3b5
sharathkumar2604 <sharathkumargs.xworkz@gmail.com>
2022-11-09 02:06:57 +0530

    check given number is +ve or -ve number

---
+++ number_posititve_negative/src/com/xworkz/number_posititve_negative/CheckPositiveOrNegative.java
@@ -0,0 +1,29 @@
+package com.xworkz.number_posititve_negative;
+
+public class CheckPositiveOrNegative {
+
+	public static void main(String[] args) {
+
+		int num=0;
+
+		if (num>0)
+		{
+		System.out.println("positive number");
+		}
+
+		else if(num<0)
+		{
+		System.out.println("negative number");
+		}
+		else 
+		{
+		System.out.println("zero number");
+		}
+		}
+		}
+
+
+
+	
+
+
